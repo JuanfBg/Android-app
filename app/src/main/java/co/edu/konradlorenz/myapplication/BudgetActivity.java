27bf4ed68eb9dc6d7bfa34ee4ea8869bcf5c1d1a
JuanfBg <bgjuan11@gmail.com>
2017-09-17 18:25:09 -0500

    New general color, Login form fixed, budget activity xml update.

--- app/src/main/java/co/edu/konradlorenz/myapplication/BudgetActivity.java
+++ app/src/main/java/co/edu/konradlorenz/myapplication/BudgetActivity.java
@@ -2,6 +2,8 @@ package co.edu.konradlorenz.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.florescu.android.rangeseekbar.RangeSeekBar;
+import android.support.design.widget.FloatingActionButton;
 
 public class BudgetActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class BudgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+        RangeSeekBar<Integer> rangeSeekBar = new RangeSeekBar<>(this);
+        // Set the range
+        rangeSeekBar.setRangeValues(2000, 10000);
+        rangeSeekBar.setSelectedMinValue(2000);
+        rangeSeekBar.setSelectedMaxValue(10000);
+
     }
 }
