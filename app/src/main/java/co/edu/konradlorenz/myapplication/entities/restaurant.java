e0103e7083d0f20502d3411e4467b87aaaa897da
kevinsv97 <kevinsv97@gmail.com>
2017-09-25 17:42:43 -0500

    created folders and restaurant ,user , commentary class

---
+++ app/src/main/java/co/edu/konradlorenz/myapplication/entities/restaurant.java
@@ -0,0 +1,15 @@
+package co.edu.konradlorenz.myapplication.entities;
+
+import java.util.ArrayList;
+
+public class restaurant {
+    String Name;
+    String Information;
+    int AveragePrice;
+    ArrayList<commentary> comments;
+    String address;
+    double latitud;
+    double longitud;
+    
+}
+
